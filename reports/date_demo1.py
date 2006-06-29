755e6a758746cf1ff271fe9c0524d550064b2920
paulproteus <paulproteus@committer.creativecommons.org>
2006-06-29 00:04:15 +0000

    hacked to use my data; still works

--- reports/date_demo1.py
+++ reports/date_demo1.py
@@ -14,6 +14,14 @@ This example requires an active internet connection since it uses
 yahoo finance to get the data for plotting
 """
 
+# Set up data
+from sqlalchemy.ext.sqlsoup import SqlSoup
+db = SqlSoup('mysql://root:@localhost/cc')
+everything = db.simple.select() # screw you, RAM!
+everything = [k for k in everything if not k.language] # need to figure out real SELECT
+everything = [k for k in everything if k.timestamp] # need to figure out real SELECT
+everything = [k for k in everything if k.count] # need to figure out real SELECT
+
 from pylab import *
 from matplotlib.finance import quotes_historical_yahoo
 from matplotlib.dates import YearLocator, MonthLocator, DateFormatter
@@ -30,8 +38,8 @@ quotes = quotes_historical_yahoo(
 if not quotes:
     raise SystemExit
 
-dates = [q[0] for q in quotes]
-opens = [q[1] for q in quotes]
+dates = [date2num(k.timestamp) for k in everything]
+opens = [k.count for k in everything]
 
 ax = subplot(111)
 plot_date(dates, opens, '-')
@@ -43,9 +51,7 @@ ax.xaxis.set_minor_locator(months)
 ax.autoscale_view()
 
 # format the coords message box
-def price(x): return '$%1.2f'%x
 ax.fmt_xdata = DateFormatter('%Y-%m-%d')
-ax.fmt_ydata = price
 
 grid(True)
 show()
