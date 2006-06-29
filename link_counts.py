8bef6e8059d928ff163a994db8f2d39aced74739
paulproteus <paulproteus@committer.creativecommons.org>
2006-06-29 18:49:43 +0000

    pychecked

--- link_counts.py
+++ link_counts.py
@@ -1,9 +1,5 @@
 from lxml import etree
-import urllib2
-import urllib
-import re
 import time
-import BeautifulSoup
 import datetime
 
 ## ADDME: MSN Search E389A2EC44FFB3F5748A9AEF7CCFED7AD82690DA and 
@@ -18,8 +14,8 @@ def debug(s):
 import google
 google.setLicense('8cJjiPdQFHK2T3LGWq+Ro04dyJr0fyZs')
 
-from simpleyahoo import legitimate_yahoo_count
 import simpleyahoo
+import lc_util
 
 from sqlalchemy.ext.sqlsoup import SqlSoup
 
@@ -52,8 +48,8 @@ class LinkCounter:
             ret.append(element.get('uri'))
         return ret # I'm not sorting.  So there.
 
-    def record(self, cc_license_uri, search_engine, count):
-        self.db.simple.insert(license_uri=cc_license_uri, search_engine=search_engine,count=count,timestamp = self.timestamp)
+    def record(self, cc_license_uri, search_engine, count, country = None, language = None):
+        self.db.simple.insert(license_uri=cc_license_uri, search_engine=search_engine,count=count,timestamp = self.timestamp, country = country, language = language)
         self.db.flush()
 
     def count_google(self):
@@ -71,17 +67,17 @@ class LinkCounter:
         # queries.  They seem to block the IP, not just the
         # user-agent.  Oops.
         for uri in self.uris:
-            self.record(cc_license_uri=uri, search_engine="All The Web", count=atw_count("link:" + uri))
-            time.sleep(0.1) # "And, breathe."
+            self.record(cc_license_uri=uri, search_engine="All The Web", count=lc_util.atw_count("link:" + uri))
+            time.sleep(1) # "And, breathe."
 
     def count_yahoo(self):
         # No sleep here because we're APIing it up.
         for uri in self.uris:
-            for language in [None] + simpleyahoo.langauges.keys():
+            for language in [None] + simpleyahoo.languages.keys():
                 for country in [None] + simpleyahoo.countries.keys():
                     countryid = simpleyahoo.countries.get(country, None) # None as default
                     langid = simpleyahoo.countries.get(language, None) # None as default
-                    count = legitimate_yahoo_count(uri, 'InlinkData', country=countryid, language=langid)
+                    count = simpleyahoo.legitimate_yahoo_count(uri, 'InlinkData', country=countryid, language=langid)
                     self.record(cc_license_uri=uri,
                                 search_engine='Yahoo',
                                 count = count,
@@ -117,7 +113,7 @@ class LinkCounter:
                     for country in [None] + simpleyahoo.countries.keys():
                         countryid = simpleyahoo.countries.get(country, None) # None as default
                         langid = simpleyahoo.countries.get(language, None) # None as default
-                        count = legitimate_yahoo_count(query=dumb_query, cc_spec=license, country=countryid, language=langid) # Query with the terse form
+                        count = simpleyahoo.legitimate_yahoo_count(query=dumb_query, cc_spec=license, country=countryid, language=langid) # Query with the terse form
                         self.record_complex(license_specifier='&'.join(license),
                                             search_engine='Yahoo',
                                             count=count,
@@ -126,7 +122,7 @@ class LinkCounter:
                                             country=country) # but store the human forms
                         
 
-    def record_complex(self, license_specifier, search_engine, count, query, country, language):
+    def record_complex(self, license_specifier, search_engine, count, query, country = None, language = None):
         self.db.complex.insert(license_specifier=license_specifier, count = count, query = query, timestamp = self.timestamp, search_engine=search_engine, country=country, language=language)
         self.db.flush()
         debug("%s gave us %d hits via %s" % (license_specifier, count, search_engine))
@@ -134,7 +130,7 @@ class LinkCounter:
 def main():
     lc = LinkCounter(dburl='mysql://root:@localhost/cc', xmlpath='old/api/licenses.xml')
     lc.count_google()
-    lc.count_alltheweb()
     lc.count_yahoo()
     lc.specific_google_counter()
     lc.specific_yahoo_counter()
+    lc.count_alltheweb() # Done last because of long sleep times
