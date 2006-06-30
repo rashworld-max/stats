38149d37b1877cd647f1f3ab52c07c6e9d3e1803
paulproteus <paulproteus@committer.creativecommons.org>
2006-06-30 00:24:19 +0000

    I am making pie charts!

--- reports/pie_charts.py
+++ reports/pie_charts.py
@@ -1,3 +1,4 @@
+import datetime
 # Jurisdictions
 # for search_engine in 'Yahoo', 'Google', 'All The Web':
 # select from simple where search_engine=search_engine
@@ -16,3 +17,58 @@
 # latest.  Be sure to get that max separately per 
 # search engine.
 
+from sqlalchemy.ext.sqlsoup import SqlSoup
+from sqlalchemy import * # Dangerously
+from pylab import * # Living
+ 
+db = SqlSoup('mysql://root:@localhost/cc')
+
+everything = db.simple.select(
+    and_(db.simple.c.timestamp != None))
+search_engines = ['Google', 'All The Web', 'Yahoo']
+
+def pie_chart(data):
+    # make a square figure and axes
+    figure(1, figsize=(8,8))
+    
+    labels = data.keys()
+    fracs = [data[k] for k in labels]
+    
+    explode=[0 for k in labels]
+    pie(fracs, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True)
+    title('Raining Hogs and Dogs', bbox={'facecolor':0.8, 'pad':5})
+    
+    show()
+    
+def extract_jurisdiction(uri):
+    splitted = uri.split('/')
+    splitted = [k for k in splitted if k]
+    numbers = [str(k) for k in range(9)]
+    last = splitted[-1]
+    for number in numbers:
+        if number in last:
+            print 'generic for', uri
+            return 'Generic'
+    if len(last) > 2:
+        print 'generic for', uri
+        return 'Generic'
+    print splitted[-1], 'for',uri
+    return splitted[-1]
+
+def jurisdiction_data():
+    # Some day, I'll do this all in SQL. :-)
+    for engine in search_engines:
+        just_us = [k for k in everything if k.search_engine == engine]
+        if not just_us:
+            print 'Hmm, nothing for', engine
+        else:
+            recent_stamp = max([k.timestamp for k in just_us])
+            recent = [k for k in just_us if k.timestamp == recent_stamp]
+
+            # Okay, now gather the data.
+            data = {}
+            for event in recent:
+                jurisdiction = extract_jurisdiction(event.license_uri)
+                data[jurisdiction] = data.get(jurisdiction, 0) + event.count
+                print 'added', event.count, 'to', jurisdiction
+            pie_chart(data)
