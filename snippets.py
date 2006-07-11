a7c356d05efae73d80f387d8eb0d9d08876c8831
paulproteus <paulproteus@committer.creativecommons.org>
2006-07-11 00:16:41 +0000

    little things that keep Asheesh happy

---
+++ snippets.py
@@ -0,0 +1,20 @@
+def list2dict(l):
+    ''' Input: a list of even length
+    Output: A dict where the odd-numbered elements map to their successors. '''
+    assert((len(l) % 2) == 0)
+    ret = {}
+    i = 0
+    while i < len(l):
+        ret[l[i]] = l[i+1]
+        i += 2
+    return ret
+
+def allthesame(l):
+    ''' Could do it recursively, but does Python do tail-call optimization? '''
+    shrunk = set(l)
+    return (len(shrunk) <= 1)
+
+def somedifferent(l):
+    shrunk = set(l)
+    return (len(shrunk) >= 2)
+
