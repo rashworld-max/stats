00aa26ef865b3c71ee69e3d8808bf84e28121045
paulproteus <paulproteus@committer.creativecommons.org>
2006-07-13 00:08:06 +0000

    added property bar chart; not sure it works.  removed "everything" variable

--- reports/charts.py
+++ reports/charts.py
@@ -11,10 +11,10 @@ import datetime
 # But it works for now, I suppose.
 # Something smarter would be to store this in the database.
 
-# FIXME: Use real SQL queries below.
-
 # FIXME: This code assumes you're always looking at the "simple"
 # table.  That's dumb.
+
+# FIXME: Remove fname parameter; calculate it each time.
 
 # Note that it should only do this for a particular 
 # run, not every single run.  Might as well get the 
@@ -28,7 +28,6 @@ import pylab, matplotlib
  
 db = SqlSoup('mysql://root:@localhost/cc')
 
-everything = db.simple.select(db.simple.c.timestamp != None)
 search_engines = ['Google', 'All The Web', 'Yahoo', 'MSN']
 all_html_colors = [k.lower() for k in ['AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black', 'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue', 'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGreen', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'Darkorange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 'DeepSkyBlue', 'DimGray', 'DodgerBlue', 'Feldspar', 'FireBrick', 'FloralWhite', 'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod', 'Gray', 'Green', 'GreenYellow', 'HoneyDew', 'HotPink', 'IndianRed', 'Indigo', 'Ivory', 'Khaki', 'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGrey', 'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateBlue', 'LightSlateGray', 'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta', 'Maroon', 'MediumAquaMarine', 'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple', 'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'Snow', 'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'VioletRed', 'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen']]
 
@@ -73,9 +72,31 @@ def get_all_urlParse_results(key):
         ret.add(r)
     return ret
 
-def pie_chart(data, title, fname):
-    # http://home.gna.org/pychart/examples/pietest.py if this gets too bad
+def bar_chart(data, title, fname):
+    labels = data.keys()
+    values = [data[k] for k in labels]
+    # Bar supports multiple, well, bars
+    # So we could do all search engines at once if we wanted
+    # But that's harder to integrate with for_search_engines
+    # So forget it.
+    ind = pylab.arange(len(values))  # the x locations for the groups
+    width = 0.35       # the width of the bars
+    pylab.p1 = pylab.bar(ind, values, width, color='r')
+    
+    pylab.ylabel('ylabel!?')
+    pylab.title(title)
+    pylab.xticks(ind+(width/2.0), labels)
+    pylab.xlim(-width,len(ind))
+    pylab.yticks(pylab.arange(0,41,10))
+    
+    pylab.show()
+    pylab.savefig(fname)
+    pylab.close()
+    
     # http://matplotlib.sourceforge.net/screenshots/barchart_demo.py shows how to smarten the legend
+    pass
+
+def pie_chart(data, title, fname):
     # make a square figure and axes
     pylab.figure(figsize=(8,8))
 
@@ -85,9 +106,9 @@ def pie_chart(data, title, fname):
     
     explode=[0.05 for k in labels]
     pylab.pie(fracs, explode=explode, labels=labels, autopct='%1.1f%%', shadow=False, colors=('b', 'g', 'r', 'c', 'm', 'y', 'w'))
-    #pylab.legend(prop=matplotlib.font_manager.FontProperties('x-small')) ## I would like this,
-    ## but legends cause duplicate labels.  Maybe fixed in upstream svn?
-    
+
+    ## I would have a legend, but they often overlap with the pie itself.
+    #pylab.legend(prop=matplotlib.font_manager.FontProperties('x-small'))
     #leg = pylab.gca().get_legend()
     #ltext  = leg.get_texts()
     #pylab.setp(ltext, fontsize='small')
@@ -96,9 +117,6 @@ def pie_chart(data, title, fname):
     pylab.title(title, bbox={'facecolor':0.8, 'pad':5})
     pylab.savefig(fname)
     pylab.close() # This is key!
-
-def bar_chart(data, title):
-    pass
 
 def date_chart(data, title):
     from matplotlib.dates import YearLocator, MonthLocator, DateFormatter
@@ -154,18 +172,6 @@ def property_counts(things):
         ret['total'] = ret.get('total',0) + thing.count
     return ret
 
-def property_bar_chart():
-    """ A whole lot of repeated code.  It's getting embarrassing. """
-    for engine in search_engine:
-        just_us = [k for k in everything if k.search_engine == engine]
-        if not just_us:
-            print 'Hmm, nothing for', engine
-        else:
-            recent_stamp = max([k.timestamp for k in just_us])
-            recent = [k for k in just_us if k.timestamp == recent_stamp ]
-            data = property_counts(things)
-            bar_chart(data, '%s Linkbacks, Property Percentages' % engine) # Does not exist! :-)
-
 def for_search_engine(chart_fn, data_fn, table):
     for engine in search_engines:
         just_us = [k for k in everything if k.search_engine == engine]
@@ -196,6 +202,17 @@ def jurisdiction_data():
     def chart_fn(data, engine):
         return pie_chart(data, "%s Jurisdiction data" % engine, "/home/paulproteus/public_html/tmp/%s.png" % engine)
 
+    for_search_engine(chart_fn, data_fn, db.simple)
+
+def property_bar_chart():
+    def data_fn(things):
+        too_much = property_counts(things)
+        if 'total' in too_much:
+            del too_much['total']
+        return too_much
+    
+    def chart_fn(data, engine):
+        return bar_chart(data, "%s property bar chart" % engine, "/home/paulproteus/public_html/tmp/%s-properties.png" % engine)
     for_search_engine(chart_fn, data_fn, db.simple)
 
 if __name__ == '__main__':
