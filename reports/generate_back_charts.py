569511cfe2be2a0b61be935f4f362b8cfd09a99d
paulproteus <paulproteus@committer.creativecommons.org>
2007-01-16 08:26:10 +0000

    most recent charts first

--- reports/generate_back_charts.py
+++ reports/generate_back_charts.py
@@ -4,11 +4,11 @@ FIRST_DATE=datetime.date(2004, 4, 1)
 TODAY=datetime.date.today()
 import charts
 
-PROCESS_THIS = FIRST_DATE
-while PROCESS_THIS < TODAY:
+PROCESS_THIS = TODAY - datetime.timedelta(days=1)
+while FIRST_DATE < PROCESS_THIS < TODAY:
     iso = PROCESS_THIS.isoformat()
     print "Going to process", iso,
     charts.main(iso)
     print "(done)."
-    PROCESS_THIS += datetime.timedelta(days=1)
+    PROCESS_THIS -= datetime.timedelta(days=1)
 
