575de50b98d36293d8d91a8239731ef774810e4e
chainlynx <chainlynx@committer.creativecommons.org>
2007-06-06 18:24:37 +0000

    Added some comments, turned the parse_uri bit into a function (for reuse in other files)

--- util/populate_typ_ver_and_juris_by_uri.py
+++ util/populate_typ_ver_and_juris_by_uri.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python
 
+"""
+A script for running through the database and updating the
+license_type, license_version and jurisdiction fields by
+parsing data out of the license_uri.
+"""
+
 import sqlalchemy
 
+def parse_fields_from_uri(uri):
+    '''parses the license_type, license_version and jurisdiction
+    from the uri and returns it in a list in that order'''
+    license_type = license_version = jurisdiction = None
+    segments = uri.split('/')
+    if len(segments) > 4:
+        license_type = segments[4]
+        if len(segments) > 5:
+            license_version = segments[5]
+            if len(segments) > 6:
+                jurisdiction = segments[6]
+    return [license_type, license_version, jurisdiction]
+ 
 # connect to the database
 dburl="mysql://dannyc:asdf@localhost/stats"
 db = sqlalchemy.create_engine(dburl)
@@ -12,27 +31,20 @@ result = text.execute()
 
 fetched = result.fetchone()
 while fetched: # while there is another row
-    id = jurisdiction = license_version = license_type = None
-    id = fetched[0]
-
-    # parse the license_uri
-    segments = fetched[1].split("/")
-    if len(segments) > 5:
-        license_type = segments[4]
-        if len(segments) > 6:
-            license_version = segments[5]
-            if len(segments) > 7:
-                jurisdiction = segments[6]
-                update_text = "UPDATE simple SET license_type = '%s', license_version = '%s', jurisdiction = '%s' WHERE id = %s" % (license_type, license_version, jurisdiction, id)
-            else:
-                update_text = "UPDATE simple SET license_type = '%s', license_version = '%s' WHERE id = %s" % (license_type, license_version, id)
-        else:
-            update_text = "UPDATE simple SET license_type = '%s' WHERE id = %s" % (license_type, id)
-    else: # nothing to update, continue to the next row
+    uri = fetched[1]
+    field_list = parse_fields_from_uri(uri)
+    if not any(field_list):
+        # nothing to update, continue to the next row
         fetched = result.fetchone() 
         continue
 
     # commit the new data
+    id = fetched[0]
+    field_list.append(id)
+    field_list = tuple(field_list)
+    update_text = "UPDATE simple SET license_type = '%s', " \
+        "license_version = '%s', jurisdiction = '%s' WHERE id = %s" \
+            % field_list
     update = db.text(update_text)
     update.execute() 
 
