1ef1150151efd48685d13f5a0f75f844d61151fe
paulproteus <paulproteus@committer.creativecommons.org>
2007-10-14 16:08:47 +0000

    removed unnecessary extra dot

--- flickr/nightly.sh
+++ flickr/nightly.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 today=$(date -I)
-wget -q http://flickr.com./creativecommons/ -O "data/$today.html" || \
+wget -q http://flickr.com/creativecommons/ -O "data/$today.html" || \
     (echo "wget failed!  Why?" ; exit 1)
 python flickr.py < "data/$today.html" > "data/$today.csv"
