88f87f9afa6136f449938e065fe9cc3a105abe98
paulproteus <paulproteus@committer.creativecommons.org>
2007-10-14 16:08:30 +0000

    silent on success, warn on fail

--- flickr/nightly.sh
+++ flickr/nightly.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 today=$(date -I)
-wget http://flickr.com/creativecommons/ -O "data/$today.html"
+wget -q http://flickr.com./creativecommons/ -O "data/$today.html" || \
+    (echo "wget failed!  Why?" ; exit 1)
 python flickr.py < "data/$today.html" > "data/$today.csv"
