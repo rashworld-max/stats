9fdc651814dd15b8906c0c8ac8a6ffaf271c08fd
paulproteus <paulproteus@committer.creativecommons.org>
2007-11-16 07:06:36 +0000

    trivial dump script; may even work

--- reports/nightly_gen_dumps.py
+++ reports/nightly_gen_dumps.py
@@ -1,2 +1,17 @@
-#!/usr/bin/FIXME
-# WRITEME!
+#!/usr/bin/python
+
+from sqlalchemy.ext.sqlsoup import SqlSoup
+import dbconfig
+import csv
+
+def main():
+    db = SqlSoup(dbconfig.dburl)
+    for table in ('simple', 'complex'):
+        out_csv = csv.writer(open(table + '.csv', 'w'))
+        table_cursor = getattr(db, table)
+        everything = table_cursor._table.select().execute()
+        for thing in everything:
+            out_csv.writerow(list(thing))
+
+if __name__ == '__main__':
+    main()
