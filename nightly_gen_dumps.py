8834733ae45f0b98d8fa0daa08e1be2af3915940
paulproteus <paulproteus@committer.creativecommons.org>
2007-12-18 21:58:38 +0000

    renaming it for easy access to dbconfig I guess

--- nightly_gen_dumps.py
+++ nightly_gen_dumps.py
@@ -3,11 +3,12 @@
 from sqlalchemy.ext.sqlsoup import SqlSoup
 import dbconfig
 import csv
+import gzip
 
 def main():
     db = SqlSoup(dbconfig.dburl)
     for table in ('simple', 'complex'):
-        out_csv = csv.writer(open(table + '.csv', 'w'))
+        out_csv = csv.writer(gzip.GzipFile(table + '.csv.gz', 'w'))
         table_cursor = getattr(db, table)
         everything = table_cursor._table.select().execute()
         for thing in everything:
