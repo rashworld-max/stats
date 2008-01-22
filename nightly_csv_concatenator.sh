6643ed34e27f8fa334dc359faadfef3e8513d00a
paulproteus <paulproteus@committer.creativecommons.org>
2008-01-22 18:06:51 +0000

    print0

--- nightly_csv_concatenator.sh
+++ nightly_csv_concatenator.sh
@@ -4,7 +4,7 @@ BASE_PATH=/home/paulproteus/csv-dumps
 for db in simple complex
 do
     file="$BASE_PATH/$db.csv"
-    find "$BASE_PATH"  -iname simple.csv | xargs cat >> "$file.work"
+    find "$BASE_PATH"  -iname simple.csv -print0 | xargs -0 cat >> "$file.work"
     mv "$file.work" "$file"
 done
 
