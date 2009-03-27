d431b0afce4094e43683fa1de2fe41c438370c09
bhy <bhy@committer.creativecommons.org>
2009-03-27 18:26:04 +0000

    Basic stats logic done. i.e. counts and percent of license and freedom scoure.

--- wikibot/ccquery.py
+++ wikibot/ccquery.py
@@ -2,6 +2,7 @@
 """
 Caculate and query CC license data.
 """
+import collections
 import sqlite3
 
 class CCQuery(object):
@@ -71,3 +72,63 @@ class CCQuery(object):
         Count of each license in a specific juris.
         """
         return self._license_query('where juris_code=?', (juris_code,))
+
+class Stats(object):
+    """
+    Caculate stats data based on an iterator which is grouped license data fetched by CCQuery.
+    The stats data includes:
+    1. absolute numbers per license and the total sum
+    2. % numbers of the above
+    3. freedom score
+
+    >>> q = CCQuery()
+    >>> q.add_linkbacks((
+    ...        ('2316360','http://creativecommons.org/licenses/by/1.0/','Google',
+    ...         '4690','2009-03-24 00:05:23','','by','1.0','Unported'),))
+
+    >>> s = Stats(q.license_world())
+    >>> s.count('by')
+    1
+    >>> s.percent('by')
+    1.0
+    >>> s.freedom_score
+    6.0
+
+    """
+    # freedom score = 6*%by+4.5*%by-sa+3*%by-nd+4*%by-nc+2.5*%by-nc-sa+1*%by-nc-nd
+    # Just tune this when needed.
+    FREEDOM_SCORE_FACTORS = ( 
+                ('by', 6),
+                ('by-sa', 4.5),
+                ('by-nd', 3),
+                ('by-nc', 4),
+                ('by-nc-sa', 2.5),
+                ('by-nc-nd', 1)
+            )
+
+    def __init__(self, license_data):
+        """
+        license_data: an iterator of (license, count) tuples
+        """
+        licenses = collections.defaultdict(int)
+        total = 0
+        for license, count in license_data:
+            licenses[license] += count
+            total += count
+
+        self.total = total
+        self._licenses = licenses
+    
+        # Calculate freedom score
+        score = 0
+        for license, factor in self.FREEDOM_SCORE_FACTORS:
+            score += self.percent(license) * factor
+        self.freedom_score = score
+        return
+    
+    def count(self, license):
+        return self._licenses[license]
+
+    def percent(self, license):
+        return float(self.count(license)) / self.total
+
