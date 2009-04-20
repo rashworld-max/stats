930c6cd397f59c23cae357e58d2eb38e86587bf1
paulproteus <paulproteus@committer.creativecommons.org>
2009-04-20 18:01:53 +0000

    Generate Mike Linksvayer-style reports nightly, for real

--- reports/nightly_ml_calculator.py
+++ reports/nightly_ml_calculator.py
@@ -5,7 +5,7 @@ FLICKR_DATA_BASE_PATH = '/home/paulproteus/stats/flickr/data/'
 OUTPUT_BASE_PATH = '/home/paulproteus/public_html/stats/ml-minimum-estimate/'
 
 ## CODE
-
+import csv
 import datetime
 import nightly_flickr_calculator
 import charts
@@ -20,33 +20,50 @@ def generate_estimates():
     for engine in charts.search_engines:
 	generate_estimate(engine, flickr_data)
 
+def cleanup_dup_keys(uri2value):
+    '''returns a copy of uri2value that doesn't have "duplicate" keys'''
+    uri2value = dict(uri2value)
+    # FIXME: Is this a good place for this sort of cleanup?
+    # when we crawl, we count linkbacks to /licenses/publicdomain *and*
+    # /licenses/publicdomain/ ! Those are "equivalent", so I take whichever
+    # is the max. Same with http://creativecommons.org/licenses/zero/1.0/
+    # and http://creativecommons.org/publicdomain/zero/1.0/
+    equivalent_uris = [
+        ('http://creativecommons.org/licenses/zero/1.0/', 'http://creativecommons.org/publicdomain/zero/1.0/'),
+        ('http://creativecommons.org/licenses/publicdomain', 'http://creativecommons.org/licenses/publicdomain/'),
+        ]
+    for uri_bunch in equivalent_uris:
+        winner_val, winner_uri = max( [ (uri2value[uri], uri) for uri in uri_bunch ] )
+        # that means drop the others
+        for loser_uri in uri_bunch:
+            if loser_uri != winner_uri:
+                del uri2value[loser_uri]
+    return uri2value
+
+def write_data_to_csv_for_engine(engine, uri2value):
+    filename = nightly_flickr_calculator.fname(engine, OUTPUT_BASE_PATH)
+    fd = open(filename, 'w')
+    csv_out = csv.writer(fd)
+    sum = 0
+
+    for uri in sorted(uri2value.keys()):
+        val = uri2value[uri]
+        csv_out.writerow( [uri, val] )
+        sum += val
+    csv_out.writerow( ['TOTAL', sum ])
+    fd.close()
+
 def generate_estimate(engine, flickr_data):
     '''Store an estimate of the total number of works, based on the Ankit
     implementation of the Giorgos method - combining search engine license
     distribution information and the Flickr data set'''
     # Flickr only refers to CC 2.0 licenses
     # Therefore, use their distribution
-    all = charts.get_all_most_recent(charts.db.simple, engine, debug = False)
-    license2num = {}
-    all_we_care_about = [ thing for thing in all if thing.license_uri in
-			  flickr2license.values() ]
-    license2num = dict(
-	[ (thing.license_uri, thing.count) for thing in all_we_care_about ])
-
-    sorted_licenses = sorted(flickr2license.values())
-    flickr_list = [ flickr_data[lic] for lic in sorted_licenses ]
-    engine_list = [ license2num[lic] for lic in sorted_licenses ]
-
-    scaled_list = cc_total_estimate_with_comments.cc_total_estimate(
-	community_list=flickr_list, search_engine_list=engine_list)
-    fd = open(fname(engine), 'w')
-    csv_out = csv.writer(fd)
-    sum = 0
-    for index, lic in enumerate(sorted_licenses):
-	csv_out.writerow([lic, scaled_list[index] ])
-	sum += scaled_list[index]
-    csv_out.writerow(['TOTAL', sum ])
-    fd.close()
+    all_as_generator = charts.get_all_most_recent(charts.db.simple, engine, debug = False)
+    data_from_engine = dict( [ (data['license_uri'], data['count']) for data in all_as_generator ] )
+    cleaned_data_from_engine = cleanup_dup_keys(data_from_engine)
+    merged_dicts = minimum_estimate.merge_dicts_max_keys(cleaned_data_from_engine, flickr_data)
+    write_data_to_csv_for_engine(engine, merged_dicts)
 
 if __name__ == '__main__':
     generate_estimates()
