b21f5364df20115dc310b8c995998b86ebbe917d
paulproteus <paulproteus@committer.creativecommons.org>
2009-04-29 23:04:29 +0000

    Add a function to calculate the proper timestamp to use based on the filename and its st_ctime

--- importers/csv2flickrdb.py
+++ importers/csv2flickrdb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import datetime
+import os
 
 # for now, copy-pasta from the reports
 # later this will be reorganized, but this is pretty one-off import code
@@ -17,6 +18,38 @@ flickr2license = {
     'http://creativecommons.org/licenses/by-sa/2.0/',
     '/creativecommons/by-nc-sa-2.0/':
     'http://creativecommons.org/licenses/by-nc-sa/2.0/'}
+
+def filename2utc_datetime(filename, _skip_stat = False, _mock_ctime = None):
+    '''Take a filename that stat()s to a real file.
+    Output a datetime object that represents a UTC timestamp according to the following rules:
+    * parse the filename to pull out an ISO date
+    * create a datetime object set to that date at noon
+    * if the file has st_ctime within 12 hours (either direction) of that stamp,
+      return the st_ctime as a datetime
+    * else return the first created datetime
+    >>> filename2utc_datetime('/nowhere/2009-03-12.csv', _mock_ctime = 0)
+    datetime.datetime(2009, 3, 12, 12, 0)
+    >>> filename2utc_datetime('/nowhere/2009-03-12.csv', _mock_ctime = 1236884413)
+    datetime.datetime(2009, 3, 12, 19, 0, 13)
+    '''
+    abspath = os.path.abspath(filename)
+    filebase = os.path.basename(abspath)
+    before_dot = filebase.rsplit('.', 1)[0]
+    # parse file name into ISO date
+    y,m,d = map(int, before_dot.split('-'))
+    ret = datetime.datetime(y, m, d, 12, 0) # unless otherwise specified
+
+    # handle the _mock_ctime for testing
+    if _mock_ctime is None:
+        ctime = os.stat(abspath).st_ctime
+    else:
+        ctime = _mock_ctime
+    # either way, calculate abs val of difference
+    from_file = datetime.datetime.utcfromtimestamp(ctime)
+    abs_val_of_diff = abs(ret - from_file)
+    if abs_val_of_diff < datetime.timedelta(hours=12):
+        ret = from_file
+    return ret
 
 def csv_row2dict(row, utc_time_stamp):
     '''
