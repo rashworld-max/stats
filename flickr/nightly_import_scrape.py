9f4e13d2af69ae43c84574b279bb6a46e338eb73
cronuser@backup <cronuser@backup@committer.creativecommons.org>
2011-06-17 20:13:39 +0000

    Make Flickr script output data to a historical dump of stats, and adjust format of daily output file.

--- flickr/nightly_import_scrape.py
+++ flickr/nightly_import_scrape.py
@@ -8,6 +8,9 @@ import argparse
 import dbconfig
 import datetime
 
+#from lxml import etree
+from lxml.html import parse
+
 flickr2license = {
     '/creativecommons/by-nd-2.0/':
         'http://creativecommons.org/licenses/by-nd/2.0/',
@@ -35,27 +38,51 @@ def parse(infd):
     return license2count
 
 def main(infd, unix_time, dry_run = False):
+    # Extract a UTC datetime object
+    utc_datetime = datetime.datetime.utcfromtimestamp(unix_time)
     # Connect to the DB; if we can't, this will fail anyway.
     db = SqlSoup(dbconfig.dburl)
     # Scrape the results we just wgetted
     license2count = parse(infd)
-    # Prepare any remaining DB columns...
+    # Prepare any remaining DB columns, and write a CSV summary file
     extra_data = {
-        'utc_time_stamp': datetime.datetime.utcfromtimestamp(unix_time),
+        'utc_time_stamp': utc_datetime,
         'site': 'http://www.flickr.com/'}
     importable = []
+    csv_values = {}
+    license2flickr = dict((v,k) for k,v in flickr2license.items())
     for key in license2count:
         row = {}
         row.update(extra_data)
         row['count'] = license2count[key]
         row['license_uri'] = key
         importable.append(row)
+        csv_values[license2flickr[key]] = row['count']
     if dry_run:
         print importable
+        print csv_values
     else:
+        # Write data to CSV file
+        csv = open('./data/%s.csv.imported' % utc_datetime.date(), 'w')
+        for license, count in csv_values.items():
+            csv.write('%s,%d\n' % (license,count))
+        csv.close()
+        # Write data to database
+        counts = {}
         for row in importable:
             db.site_specific.insert(**row)
             db.flush()
+            counts[row['license_uri'].split('/')[4]] = row['count']
+
+        # Sort by license code
+        lic_sorted = sorted(counts)
+        # Join counts (sorted) with a comma
+        cnt_sorted = ','.join(map(str, [counts[key] for key in lic_sorted]))
+        # Write csv data to big historical file.  WARNING: this presupposes
+        # that license version and jurisdiction never change on Flickr
+        hist = open('./data/counts-historical-Flickr.csv', 'a')
+        hist.write(str(utc_datetime.date()) + ',Flickr,2.0,Unported,' + cnt_sorted + '\n')
+        hist.close()
 
 if __name__ == '__main__':
     import sys
